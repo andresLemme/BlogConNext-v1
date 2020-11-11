223b1dd7efc38f6930d9218133e78a40057a22b2
Pocas1982 <andres.lemme@gmail.com>
2020-11-11 08:02:46 -0300

    SSR and CSR logic was added

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,60 @@
-import {useRouter} from 'next/router';
 import Layout from '../../Components/Layout/Layout';
 import Title from '../../Components/Title/Title';
 
-export default function PostsID(){
-  const router = useRouter();
+export default function PostsID({post}){
+  // const router = useRouter();
   // console.log(router)
- const {id} = router.query;
+//  const {id} = router.query;
   return(
     <Layout>
       <Title>Posts Details</Title>
-      <p>Posts Id {id}</p>
+      <div className="card">
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      </div>
+      <style jsx>
+      {`
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: black;
+          text-decoration: none;
+          border: 2px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+      `}
+      </style>
     </Layout>
   )
 }
+
+export async function getServerSideProps({params}){
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+  const post = await res.json();
+
+  
+  return{
+    props: {
+      post
+    }
+  }
+}
+
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,12 +1,83 @@
+import Link from "next/Link";
+import { useEffect, useState } from "react";
 import Layout from "../../Components/Layout/Layout";
-import Navbar from "../../Components/Navbar/Navbar";
 
-export default function Posts(){
+
+export default function Posts({posts}){
+//CLIENTE SIDE RENDER
+
+// const [posts, setPosts] = useState([]);
+
+// useEffect(()=>{
+//   const fetchData =  async () =>{
+//       const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//       const newPosts = await res.json()
+//       console.log(newPosts) 
+//       setPosts(newPosts)
+//   }
+//   fetchData()
+// }, [])
+
+
   return(
 
     <Layout>
-    
       <h1>Posts Page</h1>
+      <div className="grid">
+        {posts.map((post, key) =>{
+          return(
+            <Link href={`/posts/[id] `} as={`/posts/${post.id}`} key={key.id}>
+              <a className="card">
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </a>
+            </Link>
+          )
+        })}
+      </div>
+      <style jsx>
+          {`
+          .grid{
+            display:flex;
+            flex-wrap: wrap;
+            max-width: 800px;
+            margin-top: 2rem;
+          }
+          .card{
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            color: black;
+            text-decoration: none;
+            border: 2px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+          .card:hover, .card:focus, .card:active{
+            color: #0070f3;
+            border-color: #0070f3
+          } 
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+          `}
+      </style>
     </Layout>
   )
+}
+
+export async function getServerSideProps(){
+   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+   const posts = await res.json()
+  return{
+    props:{
+      posts
+    }
+  }
 }
