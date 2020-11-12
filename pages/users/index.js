1441c1d1c3c771ec21ee7592d4793089b57c2297
Pocas1982 <andres.lemme@gmail.com>
2020-11-12 08:12:15 -0300

    use of get static paths

--- pages/users/index.js
+++ pages/users/index.js
@@ -6,7 +6,7 @@ export default function Users({ users }) {
   return (
     <Layout>
       <h1>Users Page</h1>
-      <div>
+      <div className="grid">
         {users.map((user, key) => {
           return (
             <Link href={'/users/[id]'} as={`/users/${user.id}`} key={key.id}>
@@ -19,6 +19,44 @@ export default function Users({ users }) {
           );
         })}
       </div>
+      <style jsx>
+      {`
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+      `}
+      </style>
     </Layout>
   );
 }
